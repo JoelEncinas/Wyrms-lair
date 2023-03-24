e0de36fb9dd9bb2115c6ec2e4e088abad59ad404
Joel Encinas <joelencinasmartin@gmail.com>
2023-03-24 17:06:37 +0100

    added btns to js and moving functionality implemented

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,43 @@
+// Test
 import number from './test.js';
 import { url, myFunc } from "./test.js";
 
 console.log(url);
 console.log(number);
 
+// UI
 const logText = $('.log');
-logText.html('hi');
+
+const northBtn = $('.north');
+const eastBtn = $('.east');
+const southBtn = $('.south');
+const westBtn = $('.west');
+
+// Game data
+let log = '';
+
+// Utils
+function addLine(){
+    return '\n';
+}
+
+function addLog(text){
+    log += text;
+    logText.html(log);
+}
+
+const MOVE_NORTH = 'North';
+const MOVE_EAST = 'East';
+const MOVE_SOUTH = 'South';
+const MOVE_WEST = 'West';
+
+// Functionality
+northBtn.click(() => {
+    addLog(movePlayer(MOVE_NORTH));
+
+});
+
+function movePlayer(direction){
+    return `To ${direction} ${addLine()}`;
+}
+
