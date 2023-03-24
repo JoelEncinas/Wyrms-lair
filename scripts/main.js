9c457cf9607937b861296628695599a7a15dab84
Joel Encinas <joelencinasmartin@gmail.com>
2023-03-24 16:36:26 +0100

    export default test

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+import number from './test.js';
 import { url, myFunc } from "./test.js";
 
 console.log(url);
+console.log(number);
--- scripts/test.js
+++ scripts/test.js
@@ -1,6 +1,9 @@
+const number = 42;
+
 export const url = 'mypage.com';
 
 export const myFunc = a =>  {
     return a;
 }
 
+export default number;
