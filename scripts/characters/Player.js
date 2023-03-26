f2c578ce4057c64677d9307546b997c2c6892350
Joel Encinas <joelencinasmartin@gmail.com>
2023-03-26 15:50:52 +0200

    add location field to player

--- scripts/characters/Player.js
+++ scripts/characters/Player.js
@@ -1,7 +1,16 @@
 import Character from "./Character";
 
 export default class Player extends Character {
-    constructor(name){
-        super(name);
-    }
+  location;
+  constructor(name) {
+    super(name);
+  }
+
+  get location() {
+    return this.location;
+  }
+
+  set location(location){
+    this.location = location;
+  }
 }
