b26dffc7aa9febc74749e0d7c85b17564932b3df
Joel Encinas <joelencinasmartin@gmail.com>
2023-03-26 15:47:33 +0200

    fix var when loading file is null

--- scripts/main.js
+++ scripts/main.js
@@ -1,79 +1,81 @@
 // Test
-import Character from './characters/Character.js';
+import Character from "./characters/Character.js";
 
 // UI
-const playerNameText = $('.player-name');
-const logText = $('.log');
+const playerNameText = $(".player-name");
+const logText = $(".log");
 
-const northBtn = $('.north');
-const eastBtn = $('.east');
-const southBtn = $('.south');
-const westBtn = $('.west');
+const northBtn = $(".north");
+const eastBtn = $(".east");
+const southBtn = $(".south");
+const westBtn = $(".west");
 
-const eraseDataBtn = $('.erase-data');
-const loadDataBtn =  $('.load-data');
+const eraseDataBtn = $(".erase-data");
+const loadDataBtn = $(".load-data");
 
 // Game data
-let log = '';
+let log = "";
 
 // Utils
-function addLine(){
-    return '\n';
+function addLine() {
+  return "\n";
 }
 
-function addLog(text){
-    log += text;
-    logText.html(log);
+function addLog(text) {
+  log += text;
+  logText.html(log);
 }
 
-function clearData(){
-    window.localStorage.clear();
+function clearData() {
+  window.localStorage.clear();
 }
 
 eraseDataBtn.click(() => {
-    clearData();
-})
+  clearData();
+});
 
-function saveData(){
-    window.localStorage.setItem('log', log);
+function saveData() {
+  window.localStorage.setItem("log", log);
 }
 
-function loadData(){
-    log = window.localStorage.getItem('log');
-    logText.html(window.localStorage.getItem('log'));
+function loadData() {
+  if (window.localStorage.getItem("log") !== null) {
+    log = window.localStorage.getItem("log");
+    logText.html(window.localStorage.getItem("log"));
+  }
 }
 
 loadDataBtn.click(() => {
-    loadData();
-})
+  loadData();
+});
 
 // Directions
-const MOVE_NORTH = 'North';
-const MOVE_EAST = 'East';
-const MOVE_SOUTH = 'South';
-const MOVE_WEST = 'West';
+const MOVE_NORTH = "North";
+const MOVE_EAST = "East";
+const MOVE_SOUTH = "South";
+const MOVE_WEST = "West";
 
 // Move Player
 northBtn.click(() => {
-    addLog(movePlayer(MOVE_NORTH));
+  addLog(movePlayer(MOVE_NORTH));
 });
 
 eastBtn.click(() => {
-    addLog(movePlayer(MOVE_EAST));
+  addLog(movePlayer(MOVE_EAST));
 });
 
 southBtn.click(() => {
-    addLog(movePlayer(MOVE_SOUTH));
+  addLog(movePlayer(MOVE_SOUTH));
 });
 
 westBtn.click(() => {
-    addLog(movePlayer(MOVE_WEST));
+  addLog(movePlayer(MOVE_WEST));
 });
 
-function movePlayer(direction){
-    saveData();
-    return `To ${direction} ${addLine()}`;
+function movePlayer(direction) {
+  saveData();
+  return `To ${direction} ${addLine()}`;
 }
 
-let player = new Character('Jhon');
+let player = new Character("Jhon");
 playerNameText.text(player.getName);
