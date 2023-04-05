45e2b4305a4227de72c2140b0b9a99006e30f90d
Joel Encinas <joelencinasmartin@gmail.com>
2023-04-05 19:10:12 +0200

    added hbs + route

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
+const hsb = require("hbs");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
+
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+hsb.registerPartials(__dirname + "/partials");
 
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.pug2uxj.mongodb.net/?retryWrites=true&w=majority`;
 mongoose
@@ -17,5 +23,9 @@ mongoose
   })
   .then(() => console.log("Connected to DB"))
   .catch(console.error);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
