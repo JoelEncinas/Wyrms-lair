94dd2cf26da73906bea85e04a7c317979bfd9713
Joel Encinas <joelencinasmartin@gmail.com>
2023-04-11 20:43:22 +0200

    bug fix in login

--- models/User.js
+++ models/User.js
@@ -13,20 +13,4 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre("save", async function (next) {
-  const user = this;
-  if (user.isModified("password") || user.isNew) {
-    try {
-      const salt = await bcrypt.genSalt(10);
-      const hash = await bcrypt.hash(user.password, salt);
-      user.password = hash;
-      next();
-    } catch (err) {
-      return next(err);
-    }
-  } else {
-    return next();
-  }
-});
-
 module.exports = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cookieParser());
 app.use(express.static("public"));
 
 // handlebars
