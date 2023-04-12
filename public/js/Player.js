f3bac679c41a1cb6fec16155f3eec3d14eab22b0
Joel Encinas <joelencinasmartin@gmail.com>
2023-04-12 16:04:20 +0200

    created player class with properties + display on page

--- public/js/Player.js
+++ public/js/Player.js
@@ -1,6 +1,10 @@
 export class Player {
-  constructor(currentHitpoints) {
+  constructor(currentHitpoints, maximumHitpoints, gold, experience, level) {
     this._currentHitpoints = currentHitpoints;
+    this._maximumHitpoints = maximumHitpoints;
+    this._gold = gold;
+    this._experience = experience;
+    this._level = level;
   }
 
   get currentHitpoints() {
@@ -9,5 +13,37 @@ export class Player {
 
   set currentHitpoints(currentHitpoints) {
     this._currentHitpoints = currentHitpoints;
+  }
+
+  get maximumHitpoints() {
+    return this._maximumHitpoints;
+  }
+
+  set maximumHitpoints(maximumHitpoints) {
+    this._maximumHitpoints = maximumHitpoints;
+  }
+
+  get gold() {
+    return this._gold;
+  }
+
+  set gold(gold) {
+    this._gold = gold;
+  }
+
+  get experience() {
+    return this._experience;
+  }
+
+  set experience(experience) {
+    this._experience = experience;
+  }
+
+  get level() {
+    return this._level;
+  }
+
+  set level(level) {
+    this._level = level;
   }
 }
--- public/js/game.js
+++ public/js/game.js
@@ -1,5 +1,17 @@
 import { Player } from "./Player.js";
 
-var jhon = new Player(87);
+// character stats
+const hpText = document.getElementById("hit-points");
+const goldText = document.getElementById("gold");
+const experienceText = document.getElementById("experience");
+const levelText = document.getElementById("level");
 
-console.log(jhon._currentHitpoints);
+var player = new Player(10, 10, 0, 0, 1);
+
+// set character stats
+hpText.innerText = player.currentHitpoints;
+goldText.innerText = player.gold;
+experienceText.innerText = player.experience;
+levelText.innerText = player.level;
+
+console.log(player.currentHitpoints);
