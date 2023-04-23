a60e3140e4a5f9d0e3d8da29d905bddc9fdca8a1
JoelEncinas <joelencinasmartin@gmail.com>
2023-04-23 16:16:17 +0200

    added character fields to mongo model

--- models/Character.js
+++ models/Character.js
@@ -1,20 +1,55 @@
 const mongoose = require("mongoose");
 
 const characterSchema = new mongoose.Schema({
-  name: {
-    type: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "MLUser",
+    required: true,
+  },
+  currentHitPoints: {
+    type: Number,
+    required: true,
+  },
+  maximumHitPoints: {
+    type: Number,
+    required: true,
+  },
+  gold: {
+    type: Number,
+    required: true,
+  },
+  experience: {
+    type: Number,
+    required: true,
+  },
+  inventory: {
+    type: [String],
     required: true,
   },
   level: {
     type: Number,
     required: true,
   },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "MLUser",
+  quests: {
+    type: [String],
+    required: true,
+  },
+  currentLocation: {
+    type: String,
+    required: true,
+  },
+  currentWeapon: {
+    type: Number,
+    required: true,
+  },
+  currentPotion: {
+    type: Number,
+    required: true,
+  },
+  currentScroll: {
+    type: Number,
     required: true,
   },
 });
 
 module.exports = mongoose.model("WLCharacter", characterSchema);
-
