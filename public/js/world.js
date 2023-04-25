2332fe77b5c8974a0b7b853fdbbe694b9eaca953
JoelEncinas <joelencinasmartin@gmail.com>
2023-04-25 16:22:34 +0200

    added one region and more items, locations and monsters

--- public/js/world.js
+++ public/js/world.js
@@ -10,7 +10,6 @@ import { Weapon } from "./objects/Weapon.js";
 import { Scroll } from "./objects/Scroll.js";
 import { Region } from "./objects/Region.js";
 import { Craft } from "./objects/Craft.js";
-import { InventoryItem } from "./objects/InventoryItem.js";
 import { Recipe } from "./objects/Recipe.js";
 import { RecipeItem } from "./objects/RecipeItem.js";
 
@@ -27,15 +26,20 @@ export const ITEM_IDS = {
   PIECE_OF_FUR: 3,
   SNAKE_FANG: 4,
   SNAKESKIN: 5,
-  CLUB: 6,
+  BATTLEWORN_HAMMER: 6,
   HEALING_POTION: 7,
   SPIDER_FANG: 8,
   SPIDER_SILK: 9,
   ADVENTURER_PASS: 10,
-  KNIFE: 11,
+  TARNISHED_BASTARD_SWORD: 11,
   GREATER_HEALING_POTION: 12,
   SCROLL_FIREBALL_I: 13,
   SCROLL_RENEW_I: 14,
+  BRIGHT_DUST: 15,
+  BONE: 16,
+  ROLL_OF_PAPYRUS: 17,
+  SCORPION_TAIL: 18,
+  MYSTERY_MEAT: 19,
 };
 
 const UNSELLABLE_ITEM_PRICE = -1;
@@ -44,6 +48,9 @@ export const MONSTER_IDS = {
   RAT: 1,
   SNAKE: 2,
   GIANT_SPIDER: 3,
+  DUST_DEVIL: 4,
+  SKELETON: 5,
+  FIRE_SCORPION: 6,
 };
 
 export const QUEST_IDS = {
@@ -53,18 +60,27 @@ export const QUEST_IDS = {
 
 export const LOCATION_IDS = {
   HOME: 1,
-  TOWN_SQUARE: 2,
+  AVORIAS_MARKET: 2,
   GUARD_POST: 3,
   ALCHEMIST_HUT: 4,
   ALCHEMISTS_GARDEN: 5,
   FARMHOUSE: 6,
   FARM_FIELD: 7,
-  BRIDGE: 8,
+  AVORIAS_BRIDGE: 8,
   SPIDER_FIELD: 9,
+  SAND_DUNES: 10,
+  SUNSET_OASIS: 11,
+  SEA_OF_BONES: 12,
+  BURNING_WASTES: 13,
+  THE_ACID_PITS: 14,
+  THE_CRATER: 15,
+  THE_CRUMBLING_CLIFFS: 16,
+  ABANDONED_MINE: 17,
 };
 
 export const REGION_IDS = {
   ANNORA_VALLEY: 1,
+  THE_BLASTED_WASTELAND: 2,
 };
 
 export const SPELL_TYPES = {
@@ -89,46 +105,64 @@ function populateItems() {
   items.push(
     new Weapon(
       ITEM_IDS.RUSTY_SWORD,
-      "Rusty sword",
-      "Rusty swords",
+      "Rusty Sword",
+      "Rusty Swords",
       UNSELLABLE_ITEM_PRICE,
       5,
       10
     )
   );
-  items.push(new Item(ITEM_IDS.RAT_TAIL, "Rat tail", "Rat tails", 1));
+  items.push(new Item(ITEM_IDS.RAT_TAIL, "Rat Tail", "Rat Tails", 1));
   items.push(
-    new Item(ITEM_IDS.PIECE_OF_FUR, "Piece of fur", "Pieces of fur", 1)
+    new Item(ITEM_IDS.PIECE_OF_FUR, "Piece of Fur", "Pieces of Fur", 1)
   );
-  items.push(new Item(ITEM_IDS.SNAKE_FANG, "Snake fang", "Snake fangs", 1));
+  items.push(new Item(ITEM_IDS.SNAKE_FANG, "Snake Fang", "Snake Fangs", 1));
   items.push(new Item(ITEM_IDS.SNAKESKIN, "Snakeskin", "Snakeskins", 1));
-  items.push(new Weapon(ITEM_IDS.CLUB, "Club", "Clubs", 100, 8, 18));
+  items.push(
+    new Weapon(
+      ITEM_IDS.BATTLEWORN_HAMMER,
+      "Battleworn Hammer",
+      "Battleworn Hammer",
+      20,
+      7,
+      11
+    )
+  );
   items.push(
     new HealingPotion(
       ITEM_IDS.HEALING_POTION,
-      "Healing potion",
-      "Healing potions",
-      50,
+      "Healing Potion",
+      "Healing Potions",
+      10,
       40
     )
   );
-  items.push(new Item(ITEM_IDS.SPIDER_FANG, "Spider fang", "Spider fangs", 1));
-  items.push(new Item(ITEM_IDS.SPIDER_SILK, "Spider silk", "Spider silks", 1));
+  items.push(new Item(ITEM_IDS.SPIDER_FANG, "Spider Fang", "Spider Fangs", 1));
+  items.push(new Item(ITEM_IDS.SPIDER_SILK, "Spider Silk", "Spider Silks", 2));
   items.push(
     new Item(
       ITEM_IDS.ADVENTURER_PASS,
-      "Adventurer pass",
-      "Adventurer passes",
+      "Adventurer Pass",
+      "Adventurer Passes",
       UNSELLABLE_ITEM_PRICE
     )
   );
-  items.push(new Weapon(ITEM_IDS.KNIFE, "Knife", "Knives", 20, 10, 15));
+  items.push(
+    new Weapon(
+      ITEM_IDS.TARNISHED_BASTARD_SWORD,
+      "Tarnished Bastard Sword",
+      "Tarnished Bastard Swords",
+      35,
+      8,
+      12
+    )
+  );
   items.push(
     new HealingPotion(
       ITEM_IDS.GREATER_HEALING_POTION,
-      "Greater Healing potion",
-      "Greater Healing potions",
-      120,
+      "Greater Healing Potion",
+      "Greater Healing Potions",
+      25,
       100
     )
   );
@@ -137,7 +171,7 @@ function populateItems() {
       ITEM_IDS.SCROLL_FIREBALL_I,
       "Scroll: Fireball I",
       "Scrolls: Fireball I",
-      40,
+      25,
       30,
       40,
       SPELL_TYPES.DAMAGE,
@@ -149,18 +183,58 @@ function populateItems() {
       ITEM_IDS.SCROLL_RENEW_I,
       "Scroll: Renew I",
       "Scrolls: Renew I",
-      20,
+      25,
       25,
       50,
       SPELL_TYPES.HEALING,
       "Renew I"
     )
   );
+  items.push(
+    new Item(
+      ITEM_IDS.BRIGHT_DUST,
+      "Bright Dust",
+      "Bright Dust",
+      3,
+    )
+  );
+  items.push(
+    new Item(
+      ITEM_IDS.BONE,
+      "Bone",
+      "Bones",
+      2,
+    )
+  );
+  items.push(
+    new Item(
+      ITEM_IDS.ROLL_OF_PAPYRUS,
+      "Roll of Papyrus",
+      "Rolls of Papyrus",
+      3,
+    )
+  );
+  items.push(
+    new Item(
+      ITEM_IDS.SCORPION_TAIL,
+      "Scorpion Tail",
+      "Scorpion Tails",
+      1,
+    )
+  );
+  items.push(
+    new Item(
+      ITEM_IDS.MYSTERY_MEAT,
+      "Mystery Meat",
+      "Mystery Meat",
+      2,
+    )
+  );
 }
 
 function populateRecipes() {
   const fireballIRecipeComponent = new RecipeItem(
-    itemByID(ITEM_IDS.RAT_TAIL),
+    itemByID(ITEM_IDS.ROLL_OF_PAPYRUS),
     3
   );
 
@@ -215,32 +289,89 @@ function populateMonsters() {
   giantSpider.LootTable.push(
     new LootItem(itemByID(ITEM_IDS.SPIDER_SILK), 25, false)
   );
-  giantSpider.LootTable.push(new LootItem(itemByID(ITEM_IDS.CLUB), 15, false));
+
+  const dustDevil = new Monster(
+    MONSTER_IDS.DUST_DEVIL,
+    "Dust Devil",
+    10,
+    12,
+    0,
+    90,
+    90,
+    8,
+    false
+  );
+
+  dustDevil.LootTable.push(
+    new LootItem(itemByID(ITEM_IDS.BRIGHT_DUST), 100, true)
+  );
+
+  const skeleton = new Monster(
+    MONSTER_IDS.SKELETON,
+    "Skeleton",
+    13,
+    16,
+    0,
+    120,
+    120,
+    9,
+    false
+  );
+
+  skeleton.LootTable.push(new LootItem(itemByID(ITEM_IDS.BONE), 75, true));
+  skeleton.LootTable.push(
+    new LootItem(itemByID(ITEM_IDS.ROLL_OF_PAPYRUS), 20, false)
+  );
+  skeleton.LootTable.push(
+    new LootItem(itemByID(ITEM_IDS.BATTLEWORN_HAMMER), 5, false)
+  );
+
+  const fireScorpion = new Monster(
+    MONSTER_IDS.FIRE_SCORPION,
+    "Fire Scorpion",
+    15,
+    17,
+    0,
+    145,
+    145,
+    12,
+    false
+  );
+
+  fireScorpion.LootTable.push(
+    new LootItem(itemByID(ITEM_IDS.SCORPION_TAIL), 75, true)
+  );
+  fireScorpion.LootTable.push(
+    new LootItem(itemByID(ITEM_IDS.MYSTERY_MEAT), 25, false)
+  );
 
   monsters.push(rat);
   monsters.push(snake);
   monsters.push(giantSpider);
+  monsters.push(dustDevil);
+  monsters.push(skeleton);
+  monsters.push(fireScorpion);
 }
 
 function populateQuests() {
   const clearAlchemistGarden = new Quest(
     QUEST_IDS.CLEAR_ALCHEMIST_GARDEN,
-    "Clear the alchemist's garden",
-    "Kill rats in the alchemist's garden and bring back 3 rat tails. You will receive a healing potion and 10 gold pieces.",
-    10
+    "The Alchemist's Infestation",
+    "The alchemist, Master Agrippa, is in desperate need of your help! His prized garden, where he grows rare and exotic plants for his experiments, has been overrun by hordes of rats.",
+    6
   );
 
   clearAlchemistGarden.QuestCompletionItems.push(
-    new QuestCompletionItem(itemByID(ITEM_IDS.RAT_TAIL), 3)
+    new QuestCompletionItem(itemByID(ITEM_IDS.RAT_TAIL), 5)
   );
 
   clearAlchemistGarden.addRewardItems(itemByID(ITEM_IDS.HEALING_POTION));
 
   const clearFarmersField = new Quest(
     QUEST_IDS.CLEAR_FARMERS_FIELD,
-    "Clear the farmer's field",
-    "Kill snakes in the farmer's field and bring back 3 snake fangs. You will receive an adventurer's pass and 20 gold pieces.",
-    20
+    "The Farmer's Plight",
+    "A farmer by the name of John has reported a severe infestation of venomous snakes in his fields, and he fears for the safety of his family and his livestock. You have to venture into John's fields and eliminate the snake population.",
+    5
   );
 
   clearFarmersField.QuestCompletionItems.push(
@@ -254,6 +385,7 @@ function populateQuests() {
 }
 
 function populateLocations() {
+  // ANNORA
   const annoraValley = new Region(
     REGION_IDS.ANNORA_VALLEY,
     "Annora Valley",
@@ -265,95 +397,87 @@ function populateLocations() {
   const home = new Location(
     LOCATION_IDS.HOME,
     "Home",
-    "Your house. You really need to clean up the place."
+    "Few basic furnishings with a not so comfy sleeping mat. Life here is simple but often hard."
   );
 
-  const townSquare = new Location(
-    LOCATION_IDS.TOWN_SQUARE,
-    "Town square",
-    "You see a fountain."
+  const avoriasMarket = new Location(
+    LOCATION_IDS.AVORIAS_MARKET,
+    "Avoria's Market",
+    "Bustling hub of commerce and trade, where merchants and traders from all Annora converge to sell their wares. You hear a cacophony of sounds, from the clattering of horses and wagons to the chatter of vendors."
   );
 
   const bobTheRatCatcher = new Vendor("Bob the Rat-Catcher");
   bobTheRatCatcher.addItemToInventory(itemByID(ITEM_IDS.PIECE_OF_FUR), 5);
   bobTheRatCatcher.addItemToInventory(itemByID(ITEM_IDS.RAT_TAIL), 3);
-  townSquare.VendorWorkingHere = bobTheRatCatcher;
+  avoriasMarket.VendorWorkingHere = bobTheRatCatcher;
 
   const alchemistHut = new Location(
     LOCATION_IDS.ALCHEMIST_HUT,
     "Alchemist's hut",
-    "There are many strange plants on the shelves."
+    "Small, rustic structure hidden from view, obscured by thick vines, overgrown shrubs, and ancient trees. Inside, the hut is a cluttered and chaotic space, with shelves lining the walls, stacked high with jars of herbs, crystals, powders, and other arcane ingredients."
   );
   alchemistHut.QuestAvailableHere = questByID(QUEST_IDS.CLEAR_ALCHEMIST_GARDEN);
 
   const alchemistsGarden = new Location(
     LOCATION_IDS.ALCHEMISTS_GARDEN,
     "Alchemist's garden",
-    "Many plants are growing here."
+    "Plants grown here are used in the creation of potions, tinctures, and other alchemical concoctions. You are greeted by a riot of color and scent, with plants of every shape and size vying for your attention."
   );
   alchemistsGarden.MonsterLivingHere = monsterByID(MONSTER_IDS.RAT);
 
   const farmhouse = new Location(
     LOCATION_IDS.FARMHOUSE,
     "Farmhouse",
-    "There is a small farmhouse, with a farmer in front."
+    "Humble dwelling made of wood and stone, located on the outskirts of the village. It's surrounded by a small patch of land, where the farmer grows crops and raise livestock."
   );
   farmhouse.QuestAvailableHere = questByID(QUEST_IDS.CLEAR_FARMERS_FIELD);
 
   const farmersField = new Location(
     LOCATION_IDS.FARM_FIELD,
     "Farmer's field",
-    "You see rows of vegetables growing here."
+    "The crops in the field are a sight to behold. They come in a myriad of colors and shapes, ranging from towering stalks of corn to glowing orbs of enchanted fruit."
   );
   farmersField.MonsterLivingHere = monsterByID(MONSTER_IDS.SNAKE);
 
   const guardPost = new Location(
     LOCATION_IDS.GUARD_POST,
     "Guard post",
-    "There is a large, tough-looking guard here."
+    "As you approach the guard post, you can see the imposing stone walls rising up in front of you, punctuated by the wooden lookout towers on each corner. One of the guards steps forward, his hand on the hilt of his sword."
   );
-
-  guardPost.LevelRequired = 10;
 
   const bridge = new Location(
     LOCATION_IDS.BRIDGE,
-    "Bridge",
-    "A stone bridge crosses a wide river.",
+    "Stone Bridge",
+    "The bridge is old and weathered, with small cracks and uneven surfaces that have been worn down by centuries of use. You can see the water rushing below you, a crystal-clear stream that sparkles in the sunlight.",
     itemByID(ITEM_IDS.ADVENTURER_PASS)
   );
 
-  const sabanaTheMage = new Craft(
-    "Sabana the Mage",
-    recipeByID(RECIPE_IDS.CRAFT_SCROLL_FIREBALL_I)
-  );
-  bridge.CraftHere = sabanaTheMage;
-
   const spiderField = new Location(
     LOCATION_IDS.SPIDER_FIELD,
-    "Forest",
-    "You see spider webs covering covering the trees in this forest."
+    "Dark Forest",
+    "As you move deeper into the field, you can see that the ground is covered with a thick layer of spider webs, criss-crossing and overlapping in intricate patterns. "
   );
   spiderField.MonsterLivingHere = monsterByID(MONSTER_IDS.GIANT_SPIDER);
 
-  home.LocationToNorth = townSquare;
+  home.LocationToNorth = avoriasMarket;
 
-  townSquare.LocationToNorth = alchemistHut;
-  townSquare.LocationToSouth = home;
-  townSquare.LocationToEast = guardPost;
-  townSquare.LocationToWest = farmhouse;
+  avoriasMarket.LocationToNorth = alchemistHut;
+  avoriasMarket.LocationToSouth = home;
+  avoriasMarket.LocationToEast = guardPost;
+  avoriasMarket.LocationToWest = farmhouse;
 
-  farmhouse.LocationToEast = townSquare;
+  farmhouse.LocationToEast = avoriasMarket;
   farmhouse.LocationToWest = farmersField;
 
   farmersField.LocationToEast = farmhouse;
 
-  alchemistHut.LocationToSouth = townSquare;
+  alchemistHut.LocationToSouth = avoriasMarket;
   alchemistHut.LocationToNorth = alchemistsGarden;
 
   alchemistsGarden.LocationToSouth = alchemistHut;
 
   guardPost.LocationToEast = bridge;
-  guardPost.LocationToWest = townSquare;
+  guardPost.LocationToWest = avoriasMarket;
 
   bridge.LocationToWest = guardPost;
   bridge.LocationToEast = spiderField;
@@ -361,7 +485,7 @@ function populateLocations() {
   spiderField.LocationToWest = bridge;
 
   locations.push(home);
-  locations.push(townSquare);
+  locations.push(avoriasMarket);
   locations.push(guardPost);
   locations.push(alchemistHut);
   locations.push(alchemistsGarden);
@@ -371,7 +495,7 @@ function populateLocations() {
   locations.push(spiderField);
 
   home.Region = annoraValley;
-  townSquare.Region = annoraValley;
+  avoriasMarket.Region = annoraValley;
   guardPost.Region = annoraValley;
   alchemistHut.Region = annoraValley;
   alchemistsGarden.Region = annoraValley;
@@ -379,6 +503,116 @@ function populateLocations() {
   farmersField.Region = annoraValley;
   bridge.Region = annoraValley;
   spiderField.Region = annoraValley;
+
+  // ALACIA
+  const theBlastedWasteland = new Region(
+    REGION_IDS.THE_BLASTED_WASTELAND,
+    "Alacia, the Blasted Wasteland",
+    "Harsh desert wasteland that was ravaged by a magical apocalypse, now home to mutants and other dangerous creatures."
+  );
+
+  regions.push(theBlastedWasteland);
+
+  const sandDunes = new Location(
+    LOCATION_IDS.SAND_DUNES,
+    "Sand Dunes",
+    "Vast expanse of shifting sand dunes that stretch on for miles. The wind is fierce and unrelenting, and travelers can easily become lost in the endless sea of sand."
+  );
+
+  const sunsetOasis = new Location(
+    LOCATION_IDS.SUNSET_OASIS,
+    "Sunset Oasis",
+    "The oasis is a patch of lush greenery that sits amidst the endless sea of sand, surrounded by palm trees and sparkling pools of water. The sight of it is a welcome relief to travelers who have been wandering through the desert for days, struggling to survive in the blistering heat and relentless sandstorms."
+  );
+
+  const sabanaTheMage = new Craft(
+    "Sabana the Mage",
+    recipeByID(RECIPE_IDS.CRAFT_SCROLL_FIREBALL_I)
+  );
+  sunsetOasis.CraftHere = sabanaTheMage;
+
+  const seaOfBones = new Location(
+    LOCATION_IDS.SEA_OF_BONES,
+    "Sea of Bones",
+    "Bleak and barren landscape where the ground is littered with the bones of long-dead creatures. Looks haunted by restless spirits and dark magic."
+  );
+
+  const burningWastes = new Location(
+    LOCATION_IDS.BURNING_WASTES,
+    "Burning Wastes",
+    "Place of searing heat, where the sun beats down relentlessly on the parched earth, and where the sand burns like fire beneath your feet."
+  );
+
+  const theAcidPits = new Location(
+    LOCATION_IDS.THE_ACID_PITS,
+    "The Acid Pits",
+    "A series of deep pits filled with acidic sludge. The fumes from the pits are toxic and can cause severe burns and respiratory problems."
+  );
+
+  const theCrater = new Location(
+    LOCATION_IDS.THE_CRATER,
+    "The Crater",
+    "A massive impact crater caused by a meteor strike. The area is scorched and blasted, with twisted metal and debris scattered throughout."
+  );
+
+  const theCrumblingCliffs = new Location(
+    LOCATION_IDS.THE_CRUMBLING_CLIFFS,
+    "The Crumbling Cliffs",
+    "Steep cliffs that rise high above the wasteland. The edges are treacherous and unstable, and rockslides are common."
+  );
+
+  const ostaTheErmit = new Vendor("Osta the Ermit");
+  ostaTheErmit.addItemToInventory(itemByID(ITEM_IDS.PIECE_OF_FUR), 5);
+  ostaTheErmit.addItemToInventory(itemByID(ITEM_IDS.RAT_TAIL), 3);
+  theCrumblingCliffs.VendorWorkingHere = ostaTheErmit;
+
+  const abandonedMine = new Location(
+    LOCATION_IDS.ABANDONED_MINE,
+    "Abandoned Mine",
+    "A long-abandoned mine filled with dark and twisting tunnels. The air is thick with dust and the smell of sulfur, and the area is infested with dangerous creatures."
+  );
+
+  spiderField._LocationToEast = burningWastes;
+
+  sandDunes.LocationToWest = spiderField;
+
+  sunsetOasis.LocationToNorth = theAcidPits;
+  sunsetOasis.LocationToEast = sandDunes;
+  sunsetOasis.LocationToWest = seaOfBones;
+
+  seaOfBones.LocationToEast = sunsetOasis;
+  seaOfBones.LocationToWest = burningWastes;
+
+  burningWastes.LocationToEast = seaOfBones;
+  // burningWastes.LocationToWest = burningWastes;
+
+  theAcidPits.LocationToSouth = sunsetOasis;
+  theAcidPits.LocationToNorth = theCrater;
+
+  theCrater.LocationToNorth = theCrumblingCliffs;
+  theCrater.LocationToSouth = theAcidPits;
+  theCrater.LocationToWest = abandonedMine;
+
+  // theCrumblingCliffs.LocationToNorth = theCrater;
+  theCrumblingCliffs.LocationToSouth = theCrater;
+
+  locations.push(sandDunes);
+  locations.push(sunsetOasis);
+  locations.push(seaOfBones);
+  locations.push(burningWastes);
+  locations.push(theAcidPits);
+  locations.push(theCrater);
+  locations.push(theCrumblingCliffs);
+  locations.push(abandonedMine);
+
+  sandDunes.Region = theBlastedWasteland;
+  sunsetOasis.Region = theBlastedWasteland;
+  seaOfBones.Region = theBlastedWasteland;
+  burningWastes.Region = theBlastedWasteland;
+  theAcidPits.Region = theBlastedWasteland;
+  theCrater.Region = theBlastedWasteland;
+  theCrumblingCliffs.Region = theBlastedWasteland;
+  abandonedMine.Region = theBlastedWasteland;
 }
 
 export function itemByID(id) {
