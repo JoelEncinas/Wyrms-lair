054b5fd094614f43eb6cef64ed04e6f74b029dd1
JoelEncinas <joelencinasmartin@gmail.com>
2023-04-27 10:41:36 +0200

    style of auth and index routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get("/", (req, res) => {
   res.status(200).render("index");
 });
 
+router.get("/test", (req, res) => {
+  res.status(200).render("partials/main_navbar");
+});
+
 module.exports = router;
